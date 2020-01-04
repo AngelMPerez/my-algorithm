62cd4814afe5b79452a9228056296a98a308dd8c
Angel Perez <angelmperezcarranza@gmail.com>
2020-01-04 10:03:59 -0700

    testing array with numbers

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ describe('Binary Search', function() {
     describe('* Array with numbers',function(){
         describe("Item isn't in the array", function() {
             it("show a message if the item isn't in the array", function() {
-                let input = BinarySearch([1, 2, 3],4)
+                let input = BinarySearch([1, 2, 3, 5, 7, 9, 10],4)
                 let expect = 'Item not found'
 
                 assert.equal(input, expect);
@@ -13,7 +13,7 @@ describe('Binary Search', function() {
         });
         describe("Item is in the array", function() {
             it("show a message if the item is in the array", function() {
-                let input = BinarySearch([1,2,3,5,6,7,9,10,12] ,3)
+                let input = BinarySearch([1, 2, 3, 5, 6, 7, 9, 10, 12] ,3)
                 let expect = `Item found`
 
                 assert.equal(input, expect);
