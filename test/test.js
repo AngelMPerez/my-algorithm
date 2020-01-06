bea73f4ddcfe4a2a0c68a78b967db3239271b884
Angel Perez <angelmperezcarranza@gmail.com>
2020-01-06 08:34:32 -0700

    array with char

--- test/test.js
+++ test/test.js
@@ -1,24 +1,41 @@
 var assert = require('assert');
 let { BinarySearch } = require('../BinarySearch')
+
 describe('Binary Search', function() {
     describe('* Array with numbers',function(){
         describe("Item isn't in the array", function() {
-            it("show a message if the item isn't in the array", function() {
+            it("Show a message if the item isn't in the array", function() {
                 let input = BinarySearch([1, 2, 3, 5, 7, 9, 10],4)
                 let expect = 'Item not found'
 
                 assert.equal(input, expect);
-
             });
         });
         describe("Item is in the array", function() {
-            it("show a message if the item is in the array", function() {
+            it("Show a message if the item is in the array", function() {
                 let input = BinarySearch([1, 2, 3, 5, 6, 7, 9, 10, 12] ,3)
                 let expect = `Item found`
 
                 assert.equal(input, expect);
-
             });
         });
+    })
+    describe('*Array with characters', function(){
+        describe("Item isn't in the array", function(){
+            it("Show a message if the item isn't in the array", function(){
+                let input = BinarySearch(['a','b','c','d','e','f','g','h'],'p')
+                let expect = 'Item not found'
+
+                assert.equal(input, expect)
+            })
+        })
+        describe("Item is in the array", function(){
+            it("Show a message if the item is in the array", function(){
+                let input = BinarySearch(['a','b','c','d','e','f','g','h'],'g')
+                let expect = 'Item found'
+
+                assert.equal(input, expect)
+            })
+        })
     })
 });
